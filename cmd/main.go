a2fa5f3e5c0d2ad4fec728ef1e0ab19fc2084efe
Valerian Clerc <valerian.clerc@mail.mcgill.ca>
2020-02-23 02:39:24 +0100

    Script now limited to 10 repos at once. Also terminates early if no repos are retrieved

--- cmd/main.go
+++ cmd/main.go
@@ -17,11 +17,25 @@ func main() {
 	if err != nil {
 		log.Printf("Unable to get repos from challenge server: %s\n", err)
 	}
-	repos = []string{"https://github.com/Compete-McGill/techgames-sample-api.git", "https://github.com/Compete-McGill/techgames-sample-api.git", "https://github.com/Compete-McGill/techgames-sample-api.git", "https://github.com/Compete-McGill/techgames-sample-api.git", "https://github.com/Compete-McGill/techgames-sample-api.git"}
+	if len(repos) == 0 {
+		log.Println("No repos, terminating script")
+		return
+	}
+	// repos = []string{"https://github.com/Compete-McGill/techgames-sample-api.git", "https://github.com/Compete-McGill/techgames-sample-api.git", "https://github.com/Compete-McGill/techgames-sample-api.git", "https://github.com/Compete-McGill/techgames-sample-api.git", "https://github.com/Compete-McGill/techgames-sample-api.git"}
+	// repos = []string{"1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12", "13", "14", "15", "16", "17", "18", "19", "20", "21", "22", "23", "24"}
 
-	users := corrector.Setup(repos, apiURL, secret)
-	userServers := corrector.Run(users)
-	corrector.Grade(userServers)
-	corrector.Kill(userServers)
-	corrector.Clean(users)
+	for i := 0; i <= len(repos); i += 10 {
+		rem := 10
+		if len(repos) < i+10 {
+			rem = len(repos) - i
+		}
+		currRepos := repos[i : rem+i]
+		log.Printf("Running repos: %v", currRepos)
+
+		users := corrector.Setup(currRepos, apiURL, secret)
+		userServers := corrector.Run(users)
+		corrector.Grade(userServers)
+		corrector.Kill(userServers)
+		corrector.Clean(users)
+	}
 }
